86bf1abbd8ecc4f5a1e49f034a4dafc322a819e0
yml <47233769+yml66@users.noreply.github.com>
2020-11-12 13:03:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,12 +9,14 @@ function browserRedirect() {
     var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
     var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
     if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {} else {
-        document.write('<link rel="stylesheet" href="/live2d/css/live2d.css" /><div id="landlord"> <div class="message" style="opacity:0.5"></div> <canvas id="live2d" width="280" height="250" class="live2d"></canvas> <div class="hide-button"><font color="#FFFFFF">隐藏</font></div></div>');
-        document.write('<script type="text/javascript"> var message_Path = "/live2d/"</script>');
-        document.write('<script type="text/javascript"> var home_Path = "https://www.dabailuobo.com/" </script>');
-        document.write('<script type="text/javascript" src="/live2d/js/live2d.js"></script>');
-        document.write('<script type="text/javascript" src="/live2d/js/message.js"></script>');
-        document.write('<script type="text/javascript"> loadlive2d("live2d", "/live2d/model/tia/model.json");</script>');
+        document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/yml66/live2d/css/live2d.css" /><div id="landlord"> 
+      <div class="message" style="opacity:0"></div> 
+      <canvas id="live2d" width="280" height="250" class="live2d"></canvas> <div class="hide-button"><font color="#FFFFFF">隐藏</font></div></div>');
+        document.write('<script type="text/javascript"> var message_Path = 'https://cdn.jsdelivr.net/gh/yml66/live2d/'</script> ');
+        document.write('<script type="text/javascript"> var home_Path = 'https://menglei.xyz/' </script>');
+        document.write('<script data-pjax type="text/javascript" src="https://cdn.jsdelivr.net/gh/yml66/live2d/js/live2d.js"></script>');
+        document.write('<script data-pjax type="text/javascript" src="https://cdn.jsdelivr.net/gh/yml66/live2d/js/message.js"></script>');
+        document.write('<script type="text/javascript"> loadlive2d("live2d", "https://cdn.jsdelivr.net/gh/yml66/live2d/model/xiaoban/model.json"); </script> ');
     }
 }
 browserRedirect();
